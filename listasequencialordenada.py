d96522bbf87f255f3838dac3e5a559b5ea905d65
Renata <renniepires@gmail.com>
2023-09-04 15:05:41 -0300

    Added file "Lista sequencial ordenada".

---
+++ listasequencialordenada.py
@@ -0,0 +1,73 @@
+import numpy as np
+
+class Listasequencial:
+
+    def __init__(self, capacidade):
+        self.capacidade = capacidade
+        self.ultima_posicao = -1
+        self.valores = np.empty(self.capacidade, dtype=int)
+
+    def imprime(self):
+        if self.ultima_posicao == -1:
+            print('O vetor está vazio')
+        else:
+            for i in range(self.ultima_posicao + 1):
+                print(i, ' - ', self.valores[i])
+
+    def insere(self, valor):
+        if self.ultima_posicao == self.capacidade - 1:
+            print('Capacidade máxima atingida')
+            return
+        posicao = 0
+        for i in range(self.ultima_posicao + 1):
+            posicao = i
+            if self.valores[i] > valor:
+                break
+            if i == self.ultima_posicao:
+                posicao = i + 1
+
+        x = self.ultima_posicao
+        while x >= posicao:
+            self.valores[x + 1] = self.valores[x]
+            x -= 1
+        self.valores[posicao] = valor
+        self.ultima_posicao += 1
+
+    def pesquisa_linear(self, valor):
+        for i in range(self.ultima_posicao + 1):
+            if self.valores[i] > valor:
+                return -1
+            if self.valores[i] == valor:
+                return i
+            if i == self.ultima_posicao:
+                return -1
+
+    def excluir(self, valor):
+        posicao = self.pesquisa_linear(valor)
+        if posicao == -1:
+            return -1
+        else:
+            for i in range(posicao, self.ultima_posicao):
+                self.valores[i] = self.valores[i + 1]
+        self.ultima_posicao -= 1
+
+# Criando um objeto
+p1 = Listasequencial(5)
+
+# Inserindo os valores na minha lista
+print('Inserindo os valores ...\n\n')
+p1.insere(5)
+p1.insere(4)
+p1.insere(3)
+p1.insere(2)
+
+# Chamando o método imprimir
+print('Imprimindo os valores ...')
+p1.imprime()
+
+print('Pesquisando valores ...')
+print(f'A posição do valor 3 foi de {p1.pesquisa_linear(3)}')
+
+p1.excluir(3)
+print('--------------------')
+p1.imprime()
